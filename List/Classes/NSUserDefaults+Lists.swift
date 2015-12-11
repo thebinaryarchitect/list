d571789eb373bd4144847f2015923db5c016c8b3
Xiao Yao <thebinaryarchitect@gmail.com>
2015-12-11 18:25:20 -0500

    Add list persistence
    A list model is stored in the user defaults.

---
+++ List/Classes/NSUserDefaults+Lists.swift
@@ -0,0 +1,36 @@
+//
+//  NSUserDefaults+Lists.swift
+//  List
+//
+//  Copyright (C) 2015 Xiao Yao. All Rights Reserved.
+//  See LICENSE.txt for more information.
+//
+
+import Foundation
+
+// The key used to store the list in user defaults.
+let key = "com.thebinaryarchitect.ios.sample.list.savedlist"
+
+// Adds persistance functionality to lists.
+extension NSUserDefaults {
+    // Saves the list
+    func saveList(list: List) {
+        let data = NSKeyedArchiver.archivedDataWithRootObject(list)
+        self.setObject(data, forKey: key)
+    }
+    
+    // Removes the list
+    func deleteList(list: List) {
+        self.removeObjectForKey(key)
+    }
+    
+    // Loads the list
+    func loadList() -> List? {
+        if let data = self.objectForKey(key) as? NSData {
+            if let list = NSKeyedUnarchiver.unarchiveObjectWithData(data) as? List {
+                return list
+            }
+        }
+        return nil
+    }
+}

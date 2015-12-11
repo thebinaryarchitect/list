b286f5a299bcf249076063a5fc8a50ae944cc39d
Xiao Yao <thebinaryarchitect@gmail.com>
2015-12-11 18:12:56 -0500

    Define list model

--- List/Classes/List.swift
+++ List/Classes/List.swift
@@ -51,3 +51,47 @@ class ListItem : NSObject {
         self.title = title
     }
 }
+
+// Data model for a list
+class List : NSObject {
+    // A unique ID
+    let identifier: String
+    
+    // The title
+    var title = ""
+    
+    // List items
+    var items: [ListItem]
+    
+    // Initializes a list from a dictionary object.
+    // The dictionary should conform to the following structure:
+    //   identifier: String
+    //   title: String
+    //   items: [ListItem]
+    init(dictionary: NSDictionary) {
+        if let identifier = dictionary["identifier"] as? String {
+            self.identifier = identifier
+        } else {
+            self.identifier = NSUUID.init().UUIDString
+        }
+        
+        if let title = dictionary["title"] as? String {
+            self.title = title
+        }
+        
+        if let items = dictionary["items"] as? [ListItem] {
+            self.items = items
+        } else {
+            self.items = []
+        }
+        
+        super.init()
+    }
+    
+    // Initializes a list from a title.
+    init(title: String) {
+        self.identifier = NSUUID.init().UUIDString
+        self.title = title
+        self.items = []
+    }
+}

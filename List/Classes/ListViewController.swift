d7e1f250dcef47195e771e2825d28739eec3c311
Xiao Yao <thebinaryarchitect@gmail.com>
2015-12-12 16:38:35 -0500

    Change list to required

--- List/Classes/ListViewController.swift
+++ List/Classes/ListViewController.swift
@@ -12,7 +12,7 @@ import UIKit
 // Controller for a list.
 class ListViewController : UITableViewController, UITextFieldDelegate {
     // The list
-    var list: List?
+    var list: List
     
     // Initialize with a list
     init(list: List) {
@@ -22,10 +22,6 @@ class ListViewController : UITableViewController, UITextFieldDelegate {
 
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
-    }
-    
-    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: NSBundle?) {
-        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
     }
     
     override func viewDidLoad() {
@@ -45,15 +41,15 @@ class ListViewController : UITableViewController, UITextFieldDelegate {
     }
     
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.list!.items.count
+        return self.list.items.count
     }
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier(NSStringFromClass(UITableViewCell.self), forIndexPath: indexPath)
         
-        let item = self.list!.items[indexPath.row]
+        let item = self.list.items[indexPath.row]
         cell.textLabel?.text = item.title
-        cell.accessoryType = item.completed ? UITableViewCellAccessoryType.Checkmark : UITableViewCellAccessoryType.None
+        cell.accessoryType = item.completed ? .Checkmark : .None
         
         return cell
     }
@@ -64,8 +60,8 @@ class ListViewController : UITableViewController, UITextFieldDelegate {
     
     override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
         if editingStyle == UITableViewCellEditingStyle.Delete {
-            self.list!.items.removeAtIndex(indexPath.row)
-            self.list!.save()
+            self.list.items.removeAtIndex(indexPath.row)
+            self.list.save()
             tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
         }
     }
@@ -75,10 +71,9 @@ class ListViewController : UITableViewController, UITextFieldDelegate {
     }
     
     override func tableView(tableView: UITableView, moveRowAtIndexPath sourceIndexPath: NSIndexPath, toIndexPath destinationIndexPath: NSIndexPath) {
-        let item = self.list!.items[sourceIndexPath.row]
-        self.list!.items.removeAtIndex(sourceIndexPath.row)
-        self.list!.items.insert(item, atIndex: destinationIndexPath.row)
-        self.list!.save()
+        let item = self.list.items[sourceIndexPath.row]
+        self.list.items.insert(item, atIndex: destinationIndexPath.row)
+        self.list.save()
     }
     
     // MARK: Private
@@ -86,9 +81,9 @@ class ListViewController : UITableViewController, UITextFieldDelegate {
     func addItem(text: String) {
         if !text.isEmpty {
             let item = ListItem.init(title: text)
-            self.list!.items.append(item)
-            self.list!.save()
-            let indexPath = NSIndexPath.init(forRow: self.list!.items.count-1, inSection: 0)
+            self.list.items.append(item)
+            self.list.save()
+            let indexPath = NSIndexPath.init(forRow: self.list.items.count-1, inSection: 0)
             self.tableView.insertRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
         }
     }
@@ -97,9 +92,9 @@ class ListViewController : UITableViewController, UITextFieldDelegate {
     
     override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
         tableView.deselectRowAtIndexPath(indexPath, animated: true)
-        let item = self.list!.items[indexPath.row]
+        let item = self.list.items[indexPath.row]
         item.completed = !item.completed
-        self.list!.save()
+        self.list.save()
         let cell = tableView.cellForRowAtIndexPath(indexPath)
         cell?.accessoryType = item.completed ? .Checkmark : .None
     }

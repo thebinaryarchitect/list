0727d9b77687eb9b3f9ff641c703f804f958d13e
Xiao Yao <thebinaryarchitect@gmail.com>
2015-12-12 00:40:41 -0500

    Implement data source

--- List/Classes/ListViewController.swift
+++ List/Classes/ListViewController.swift
@@ -12,7 +12,7 @@ import UIKit
 // Controller for a list.
 class ListViewController : UITableViewController, UITextFieldDelegate {
     // The list
-    var list: List?
+    var list: List
     
     // Initialize with a list
     init(list: List) {
@@ -36,5 +36,46 @@ class ListViewController : UITableViewController, UITextFieldDelegate {
         textField.delegate = self
         textField.placeholder = "Add List Item"
         self.navigationItem.titleView = textField
+    }
+    
+    // MARK: UITableViewDataSource
+    
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.list.items.count
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier(NSStringFromClass(UITableViewCell.self), forIndexPath: indexPath)
+        
+        let item = self.list.items[indexPath.row]
+        cell.textLabel?.text = item.title
+        cell.accessoryType = item.completed ? UITableViewCellAccessoryType.Checkmark : UITableViewCellAccessoryType.None
+        
+        return cell
+    }
+    
+    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        return true
+    }
+    
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == UITableViewCellEditingStyle.Delete {
+            self.list.items.removeAtIndex(indexPath.row)
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+        }
+    }
+    
+    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        return true
+    }
+    
+    override func tableView(tableView: UITableView, moveRowAtIndexPath sourceIndexPath: NSIndexPath, toIndexPath destinationIndexPath: NSIndexPath) {
+        let item = self.list.items[sourceIndexPath.row]
+        self.list.items.removeAtIndex(sourceIndexPath.row)
+        self.list.items.insert(item, atIndex: destinationIndexPath.row)
     }
 }

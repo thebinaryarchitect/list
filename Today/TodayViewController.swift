7d8e2ec5238cdc35f3d3a6f5630e6585d9233728
Xiao Yao <thebinaryarchitect@gmail.com>
2015-12-13 15:00:30 -0500

    Implement today VC data source

--- Today/TodayViewController.swift
+++ Today/TodayViewController.swift
@@ -20,6 +20,7 @@ class TodayViewController: UITableViewController, NCWidgetProviding {
             self.list = List.init(title: "")
         }
         
+        print(self.list.items)
         super.init(style: style)
     }
 
@@ -29,6 +30,12 @@ class TodayViewController: UITableViewController, NCWidgetProviding {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.preferredContentSize = CGSizeMake(0.0, CGFloat(self.list.items.count) * 44.0)
+        
+        self.tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: NSStringFromClass(UITableViewCell.self))
+        
+        print(self.preferredContentSize)
     }
     
     // MARK: NCWidgetProviding
@@ -40,5 +47,50 @@ class TodayViewController: UITableViewController, NCWidgetProviding {
         // If there's no update required, use NCUpdateResult.NoData
         // If there's an update, use NCUpdateResult.NewData
         completionHandler(NCUpdateResult.NewData)
+    }
+    
+    // MARK: UITableViewDataSource
+    
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.list.items.count
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier(NSStringFromClass(UITableViewCell.self), forIndexPath: indexPath)
+        
+        let item = self.list.items[indexPath.row]
+        cell.textLabel?.text = item.title
+        cell.accessoryType = item.completed ? .Checkmark : .None
+        
+        return cell
+    }
+    
+    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        return true
+    }
+    
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == UITableViewCellEditingStyle.Delete {
+            self.list.items.removeAtIndex(indexPath.row)
+            self.list.save()
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+        }
+    }
+    
+    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        return true
+    }
+    
+    override func tableView(tableView: UITableView, moveRowAtIndexPath sourceIndexPath: NSIndexPath, toIndexPath destinationIndexPath: NSIndexPath) {
+        if sourceIndexPath.row != destinationIndexPath.row {
+            let item = self.list.items[sourceIndexPath.row]
+            self.list.items.removeAtIndex(sourceIndexPath.row)
+            self.list.items.insert(item, atIndex: destinationIndexPath.row)
+            self.list.save()
+        }
     }
 }

32e8a93f16fbe6d763903e95d0c3bd6649b62c44
Xiao Yao <thebinaryarchitect@gmail.com>
2015-12-13 14:42:24 -0500

    Override initializer

--- Today/TodayViewController.swift
+++ Today/TodayViewController.swift
@@ -8,13 +8,30 @@
 
 import UIKit
 import NotificationCenter
+import List
 
 class TodayViewController: UITableViewController, NCWidgetProviding {
+    let list: List
     
+    override init(style: UITableViewStyle) {
+        if let list = NSUserDefaults.standardUserDefaults().loadList() {
+            self.list = list
+        } else {
+            self.list = List.init(title: "")
+        }
+        
+        super.init(style: style)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    // MARK: NCWidgetProviding
         
     func widgetPerformUpdateWithCompletionHandler(completionHandler: ((NCUpdateResult) -> Void)) {
         // Perform any setup necessary in order to update the view.
@@ -24,5 +41,4 @@ class TodayViewController: UITableViewController, NCWidgetProviding {
         // If there's an update, use NCUpdateResult.NewData
         completionHandler(NCUpdateResult.NewData)
     }
-    
 }
